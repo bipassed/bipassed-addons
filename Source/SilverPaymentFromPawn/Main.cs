d43d8eab821e00413df19c682d4b8bec737870d8
bipassed <ocarina0001@gmail.com>
2023-06-30 02:56:37 -0400

    More file rework

---
+++ Source/SilverPaymentFromPawn/Main.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace BipassedAddons
+{
+	[StaticConstructorOnStartup]
+	public class Main
+	{
+		static Main()
+		{
+			new Harmony("bipassed.addons").PatchAll(Assembly.GetExecutingAssembly());
+		}
+	}
+
+}
--- Source/SilverPaymentFromPawn/VariousDefOf.cs
+++ Source/SilverPaymentFromPawn/VariousDefOf.cs
@@ -1,13 +1,16 @@
-﻿using System;
+﻿using RimWorld;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Verse;
 
 namespace BipassedAddons
 {
-    public class SilverPaymentDrop
+    [DefOf]
+    public static class VariousDefOf
     {
-
+        public static ThingDef Silver;
     }
 }
